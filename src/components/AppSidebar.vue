d67729cca2079cfac1d5487c33d9763ff1605e4b
Nikola <meljames.cortes@jmc.edu.ph>
2025-08-20 15:35:42 +0800

    commit

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,172 @@
+<!-- <script setup lang="ts">
+import type { SidebarProps } from '@/components/ui/sidebar'
+
+import {
+  AudioWaveform,
+  Command,
+
+  GalleryVerticalEnd,
+  SquareTerminal,
+} from "lucide-vue-next"
+import NavMain from '@/components/NavMain.vue'
+import NavUser from '@/components/NavUser.vue'
+import TeamSwitcher from '@/components/TeamSwitcher.vue'
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from '@/components/ui/sidebar'
+
+const props = withDefaults(defineProps<SidebarProps>(), {
+  collapsible: "icon",
+})
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Playground",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "Dashboard",
+          url: "#",
+        },
+        {
+          title: "Admin Management",
+          url: "#",
+        },
+        {
+          title: "Driver Management",
+          url: "#",
+        },
+        {
+          title: "Route Management",
+          url: "#",
+        },
+        {
+          title: "Reports & Logs",
+          url: "#",
+        },
+        {
+          title: "Backups",
+          url: "#",
+        },
+        {
+          title: "Audits Logs",
+          url: "#",
+        },
+      ],
+    },
+    
+  ],
+}
+</script>
+
+<template>
+  <Sidebar v-bind="props">
+    <SidebarHeader>
+      <TeamSwitcher :teams="data.teams" />
+    </SidebarHeader>
+    <SidebarContent>
+      <NavMain :items="data.navMain" />
+    </SidebarContent>
+    <SidebarFooter>
+      <NavUser :user="data.user" />
+    </SidebarFooter>
+    <SidebarRail />
+  </Sidebar>
+</template> -->
+
+
+<script setup lang="ts">
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-vue-next"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+</script>
+
+<template>
+  <Sidebar>
+    <SidebarContent>
+      <SidebarGroup>
+        <SidebarGroupLabel>Application</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+              <SidebarMenuItem v-for="item in items" :key="item.title">
+                <SidebarMenuButton asChild>
+                    <a :href="item.url">
+                      <component :is="item.icon" />
+                      <span>{{item.title}}</span>
+                    </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
+  </Sidebar>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import router from './router'
+import Vue from 'vue'
+
+new Vue ({
+    router
+})
